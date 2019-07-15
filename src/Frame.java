98db7c369a79947bfe019f10dc2f42bfd27e008a
rlo19 <ochengcoraymond@gmail.com>
2019-07-15 16:38:47 -0700

    file reader done

--- src/Frame.java
+++ src/Frame.java
@@ -1,13 +1,24 @@
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingWorker;
+import javax.swing.border.Border;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Frame {
 
@@ -15,6 +26,7 @@ public class Frame {
 	JButton btnRead, btnUpload;
 	JTextArea textArea;
 	JPanel panel;
+	JScrollPane scrollPane;
 	
 	Teller teller;
 	
@@ -23,11 +35,21 @@ public class Frame {
 		frame = new JFrame("StoryTeller");
 		frame.setSize(450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);			
+		frame.getContentPane().setLayout(null);	
+		
 		
 		textArea = new JTextArea();
-		textArea.setBounds(25, 27, 381, 135);
-		frame.getContentPane().add(textArea);
+		textArea.setLineWrap(true);
+		textArea.setWrapStyleWord(true);
+		Border border = BorderFactory.createLineBorder(Color.BLACK);
+		textArea.setBorder(BorderFactory.createCompoundBorder(border, 
+		            BorderFactory.createEmptyBorder(10, 10, 10, 10)));
+				
+		scrollPane = new JScrollPane();
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setBounds(29, 11, 369, 170);
+		frame.getContentPane().add(scrollPane);
+		scrollPane.setViewportView(textArea);
 		
 		btnRead = new JButton("Read");
 		btnRead.setBounds(88, 195, 89, 23);
@@ -59,6 +81,9 @@ public class Frame {
 				
 				JFileChooser fileChooser = new JFileChooser();
 				
+				FileNameExtensionFilter filter = new FileNameExtensionFilter("TEXT FILES", "txt", "text");
+				fileChooser.setFileFilter(filter);
+				
 				int returnValue = fileChooser.showOpenDialog(null);
 				
 		        if (returnValue == JFileChooser.APPROVE_OPTION) {
@@ -70,7 +95,6 @@ public class Frame {
 		        	 Writer writer = new Writer(selectedFile);
 		        	 
 		        	 writer.execute();
-		        	 System.out.println("Frame get " + writer.getPropertyChangeSupport());
 		          }		          		          
 		        }
 			}
@@ -91,5 +115,85 @@ public class Frame {
 		teller.cancel(true);
 		btnRead.setText("Read");
 		teller = new Teller("Hello World");
+	}
+	
+	private class Writer extends SwingWorker<Void, String> {
+
+		File file;
+		FileInputStream inputStream = null;
+		Scanner sc = null;
+		
+		@Override
+		protected Void doInBackground() throws Exception {
+			
+			write();
+			
+			return null;
+		}
+		
+		protected Writer(File file) {
+			
+			this.file = file;
+		}
+		
+		private void write() throws IOException {
+
+			try {
+				
+				inputStream = new FileInputStream(this.file.getPath());
+				
+				sc = new Scanner(inputStream, "UTF-8");
+				
+				while(sc.hasNextLine()) {
+					
+					System.out.println("publish");
+					
+					publish(sc.nextLine());
+
+					Thread.sleep(2000);
+				}
+				
+				if (sc.ioException() != null) {
+			        throw sc.ioException();
+			    }
+				
+			} catch (Exception e) {
+				
+				System.out.println("err");
+				System.err.println(e);
+				
+			} finally {
+				
+			    if (inputStream != null) {
+			    	
+			        inputStream.close();		        
+			    }
+			    
+			    if (sc != null) {
+			    	
+			        sc.close();
+			    }
+			}
+		}
+		
+		@Override
+		protected void process(List<String> chunks) { 
+            
+			String val = chunks.get(chunks.size()-1); 
+			
+			if(val.isEmpty()) {
+				val = "\n\n";
+				
+			}
+			System.out.println(String.valueOf(val));
+            
+            textArea.append(String.valueOf(val));
+        } 
+		
+		@Override
+		protected void done() {
+			
+			System.out.println("Done writing");			
+		}
 	}
 }
